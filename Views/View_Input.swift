07061b2a92f890a67eff22940cb1f19c8aa4ecd9
Sedat Onat <sedatonat@outlook.com>
2023-01-29 18:15:36 +0300

    Update
    New Folder Arrengements

---
+++ Views/View_Input.swift
@@ -0,0 +1,20 @@
+//
+//  View_Input.swift
+//  SOPS001X
+//
+//  Created by Sedat Onat on 29.01.2023.
+//
+
+import SwiftUI
+
+struct View_Input: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct View_Input_Previews: PreviewProvider {
+    static var previews: some View {
+        View_Input()
+    }
+}
