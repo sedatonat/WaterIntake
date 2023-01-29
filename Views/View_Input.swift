e5f87d14551ccc2b26486e7446a321442b690242
Sedat Onat <sedatonat@outlook.com>
2023-01-29 20:43:51 +0300

    update
    picker added but it does not add to the texfield. it only changes the number in textfield

--- Views/View_Input.swift
+++ Views/View_Input.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 struct View_Input: View {
-    @State private var addWaterCustom01 = 0
-    @State private var addWaterCustom02 = 0
-    @State private var addWaterCustom03 = 0
+    @State var waterCups = [100,200,300,400]
+    // stride(from: 100, to: 400, by: 100)
+    
     @State private var addWater = 0
     
     var body: some View {
@@ -18,24 +18,41 @@ struct View_Input: View {
         NavigationView {
             
             VStack {
-                Button(action: {self.addWater = addWater + 200}) {
-                    Text("Add 200")
-                }
+                
+                Picker("Add Water", selection: $addWater) {
+                    ForEach(waterCups, id: \.self) {
+                        Text($0, format: .number)
+                    }
+                } // End of Picker
+                .pickerStyle(.segmented)
+                
+//                Button(action: {self.addWater = addWater + 200}) {
+//                    Text("Add 200")
+//                } // End of Button
+            
+                
+                
                 
                 Form {
                     
-                    Section {
-                        TextField("Add Water", value: $addWater, format: .number)
-                            .foregroundColor(.primary)
-                    } header: {
-                        Text("Add Water")
-                    }
+
+                
+                        Section {
+                            TextField("Add Water", value: $addWater, format: .number)
+                                .foregroundColor(.primary)
+                        } header: {
+                            Text("Add Water")
+                        }
+                        // End of Section
+                        
                     
-                }
-            }
-        }
-    }
-}
+                    
+                    
+                } // End of Form
+            } // End of VStack
+        } // End of NavigationView
+    } // End of some View
+} // End of Main View
 
 struct View_Input_Previews: PreviewProvider {
     static var previews: some View {
