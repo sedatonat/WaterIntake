2f195e5199604240dbedd85bfb72acea0791af58
Sedat Onat <sedatonat@outlook.com>
2023-01-30 05:08:49 +0300

    update
    keyboard added
    could not grab the old value in order to add to the new one but I will solve it

--- Views/View_Input.swift
+++ Views/View_Input.swift
@@ -5,13 +5,17 @@
 //  Created by Sedat Onat on 29.01.2023.
 //
 
+
 import SwiftUI
 
 struct View_Input: View {
-    @State var waterCups = [100,200,300,400]
+    let waterCups = [100,200,300,400]
     // stride(from: 100, to: 400, by: 100)
     
     @State private var waterIntake = 0
+    @State private var waterIntakeOldvalue = 0
+    @FocusState private var xxxIsFocused: Bool
+    
     
     var body: some View {
         NavigationView {
@@ -19,6 +23,10 @@ struct View_Input: View {
                 VStack {
                     
                     Section {
+                        
+                        let waterIntakeOldvalue = waterIntake
+                        
+                        
                         Picker("Choose the amount", selection: $waterIntake) {
                             ForEach(waterCups, id: \.self) {
                                 Text($0, format: .number)
@@ -27,31 +35,60 @@ struct View_Input: View {
                         .pickerStyle(.segmented)
                         
                         
-//                        .onTapGesture {
-//                            self.addWater = addWater + 200
-//                        } // onTapGesture adds 200
-                       
                         
+//                        .onChange(of: waterIntake) { [waterIntake] newState in
+//                            print(waterIntake, self.waterIntake, newState)
+//                        } // hatasiz
                         
-                         // End of Button
                         
                         Button(action: {self.waterIntake = 0}) {
                             Text("Reset")
                         } // End of Button
 
+//                        Text("\(waterCups)" as String)
+
+                        
+                        
+
+                        
+                        
+                        //                        .onTapGesture {
+    //                            self.waterIntake = waterIntake + 10
+    //                        } // onTapGesture adds 200
 
                     } header: { Text("Pick the amount") }
                     
                     
+                    
+                    
                     Form {
                         Section {
+                            Text("old value is \(waterIntakeOldvalue)")
                             TextField("Drink Water", value: $waterIntake, format: .number)
                                 .foregroundColor(.primary)
+                                .keyboardType(.numberPad)
+                                .focused($xxxIsFocused) // trigers the state
                         } header: { Text("Daily Water Intake") }
+                            
+                        
                         // End of Section
                     } // End of Form
                 } // End of VStack
+            
+                
+                
+                .navigationTitle("Daily Water Intake")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer() // brought Done to the right side
+                        Button("Done") {
+                            xxxIsFocused = false
+                        }
+                    }
+                }
+            
         } // End of NavigationView
+        
     } // End of some View
 } // End of Main View
 
