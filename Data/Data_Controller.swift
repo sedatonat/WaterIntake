c51dc5c639d220500fb98152bb1d6004b16bdb92
Sedat Onat <sedatonat@outlook.com>
2023-01-30 21:30:25 +0300

    update
    Cloudkit settings

---
+++ Data/Data_Controller.swift
@@ -0,0 +1,15 @@
+//
+//  Data_Controller.swift
+//  SOPS001X
+//
+//  Created by Sedat Onat on 30.01.2023.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    
+    
+    
+}
